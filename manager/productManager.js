c2702296203d0e4af0974f5731f44366e8cfbb2f
Ernesto Rosas <er.rosas24@gmail.com>
2023-12-02 11:11:58 -0300

    desafio entregable/01

---
+++ manager/productManager.js
@@ -0,0 +1,61 @@
+class ProductManager {
+    constructor() {
+    this.products = [];
+    this.productId = 1;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+    // Validar campos obligatorios
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.error("Todos los campos son obligatorios");
+        return;
+    }
+
+    // Validar que no se repita el campo "code"
+    const existingProduct = this.products.find((product) => product.code === code);
+    if (existingProduct) {
+        console.log("Ya existe un producto con ese código");
+        return;
+    }
+
+    // Crear el producto con un id autoincrementable
+    const newProduct = {
+        id: this.productId,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+    };
+
+    // Incrementar el id para el próximo producto
+    this.productId++;
+
+    // Agregar el producto al arreglo de productos
+    this.products.push(newProduct);
+    console.log("Producto agregado:", newProduct);
+    }
+
+    getProducts() {
+    return this.products;
+    }
+
+    getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+        console.error("Producto no encontrado");
+    }
+    return product;
+    }
+}
+
+// Ejemplo de uso
+const manager = new ProductManager();
+
+manager.addProduct("Producto 1", "Descripción del Producto 1", 20, "imagen1", "abc123", 10);
+manager.addProduct("Producto 2", "Descripción del Producto 2", 30, "imagen2", "abc124", 20);
+
+console.log("Todos los productos:", manager.getProducts());
+console.log("Producto con id 2:", manager.getProductById(2));
+console.log("Producto con id 5:", manager.getProductById(5)); // Debería mostrar un error
