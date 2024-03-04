26d846856b02969020f80795216465fd53356477
Ernesto Rosas <er.rosas24@gmail.com>
2024-03-04 04:17:55 -0300

    desafio entregable/09/clase 14/Algún código comentado borrado

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,7 +15,6 @@ export const configObject = {
     jwt_secret_Key: process.env.JWT_SECRET_KEY
 }
 
-
 export const connectDB = async () => {
     try {
         await MongoSingleton.getInstance(process.env.MONGO_URL)      
@@ -25,14 +24,3 @@ export const connectDB = async () => {
         console.log(error)
     }
 }
-
-
-// export const connectDB = async ( ) => {
-//     try {
-//         await connect('mongodb+srv://errosas24:QvBOVYSG5ndTvKTr@cluster0.o1tsu31.mongodb.net/backendCoder50010?retryWrites=true&w=majority')
-//         // await connect('mongodb://127.0.0.1:27017/backendCoder50010')
-//         console.log('base de datos connected')
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import UserController from '../controllers/users.controller.js';
-// import userModel from '../daos/models/user.model.js'
-// import UserManagerMongo from '../daos/mongo/userManagerMongo.js';
 import { passportCall } from '../middleware/pasportCall.js';
 import { authorization } from '../middleware/authentication.js';
 
-
 const router = express.Router();
-
-// const userService = new UserManagerMongo()
 
 const {
     getUsers,
@@ -25,92 +20,5 @@ router.post('/', createUser);
 router.put('/:uid', updateUser)
 router.delete('/:uid', deleteUser)
 router.get('/allusers', getAllUser);
-
-
-//, passportCall('jwt'), authorization( ['USER_PREMIUM', 'ADMIN'] )
-// router.get('/', async (request, responses)=>{
-//         try {
-//             const {limit = 5, pageQuery = 1} = request.query
-//             const {
-//                 docs,
-//                 hasPrevPage, 
-//                 hasNextPage,
-//                 prevPage, 
-//                 nextPage,
-//                 page 
-//                 } = await userService.getUsersPaginate(limit, pageQuery)
-//                 responses.render('users', {
-//                     users: docs,
-//                     hasPrevPage, 
-//                     hasNextPage,
-//                     prevPage, 
-//                     nextPage,
-//                     page,
-//                 })
-//             } catch (error) {
-//                 console.log(error)
-//             }
-//     })
-
-
-
-
-
-    // .post('/', async (request, responses)=>{
-    //     try {
-    //         const { body } = request
-    //         const result = await userService.createUser(body)
-
-    //         responses.send({
-    //             status: 'success',
-    //             result
-    //         })
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // })
-    // .get('/:uid', async (request, responses)=>{
-    //     try {
-    //         const { uid } = request.params
-    //         const user = await userService.getUser({_id: uid})
-    //         responses.json({
-    //             status: 'success',
-    //             result: user
-    //         })
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // })
-    // .put('/:uid', async (request, responses)=>{
-    //     try {
-    //         const { uid } = request.params;
-    //         const { body } = request;
-            
-    //         // Aquí podrías validar que hay datos en el body antes de intentar actualizar el usuario
-            
-    //         const result = await userService.updateUser(uid, body);
-            
-    //         responses.send({
-    //             status: 'success',
-    //             message: 'User updated successfully',
-    //             result
-    //         });
-    //     } catch (error) {
-    //         console.log(error);
-    //         responses.status(500).send({
-    //             status: 'error',
-    //             message: 'An error occurred while updating the user'
-    //         });
-    //     }
-    // })
-    // .delete('/:uid', async (request, responses)=>{
-    //     try {
-    //         const {uid} = request.params
-    //         const result = await userService.deleteUser(uid)
-    //         responses.send('delete user')
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // })
 
 export default router;
